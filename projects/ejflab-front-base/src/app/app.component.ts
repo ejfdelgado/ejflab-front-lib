6914820c9c6dc81d517c7992da2b1043e50b710e
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2024-10-13 20:25:29 -0500

    tested

--- projects/ejflab-front-base/src/app/app.component.ts
+++ projects/ejflab-front-base/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EjflabFrontLibComponent } from 'ejflab-front-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EjflabFrontLibComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ejflab-front-base';
