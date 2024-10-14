e42b6d706195841c9749bf6c9ebc217b93ee4ae0
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2024-10-13 20:49:11 -0500

    fixing dependencies

--- projects/ejflab-front-base/src/app/app.component.ts
+++ projects/ejflab-front-base/src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { EjflabFrontLibComponent } from 'ejflab-front-lib';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, EjflabFrontLibComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ejflab-front-base';
+  constructor() {
+    document.cookie = 'yo=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  }
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    //console.log(event);
+    if (event.ctrlKey) {
+      switch (event.code) {
+        case 'KeyI':
+          break;
+        case 'KeyQ':
+          break;
+        case 'KeyB':
+          break;
+        case 'Comma':
+          break;
+        case 'Period':
+          break;
+        case 'Enter':
+          break;
+        case 'Backquote':
+          //El pipe | arriba a la izquierda
+          break;
+        case 'NumpadEnter':
+          break;
+        default:
+          console.log(event.code);
+      }
+    }
+  }
 }
