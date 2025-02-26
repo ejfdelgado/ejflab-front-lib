5381c080b6840aa8efa845497fea5e963fb5dcf1
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-02-26 06:51:53 -0500

    added provider

--- projects/ejflab-front-lib/src/lib/mycommon/services/modalError.service.ts
+++ projects/ejflab-front-lib/src/lib/mycommon/services/modalError.service.ts
@@ -1,16 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, Optional } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AlertComponent } from '../components/alert/alert.component';
+
+export abstract class ErrorManager {
+  abstract errorToMessage(error: Error): Promise<{ title: string, txt: string }>;
+}
+
+export class ErrorManagerDefault extends ErrorManager {
+  override async errorToMessage(error: Error): Promise<{ title: string; txt: string; }> {
+    return { title: "Oops!", txt: error.message };
+  }
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ModalErrorService {
-  constructor(public dialog: MatDialog) {}
+  defaultErrorManager = new ErrorManagerDefault();
+  constructor(
+    public dialog: MatDialog,
+    @Optional() @Inject('errorManager') private errorManager: ErrorManager,
+  ) { }
 
   async error(error: Error) {
+
+    let message = {};
+    if (this.errorManager) {
+      // Provided
+      message = await this.errorManager.errorToMessage(error);
+    } else {
+      // Default
+      message = await this.defaultErrorManager.errorToMessage(error);
+    }
     const dialogRef = this.dialog.open(AlertComponent, {
-      data: { title: 'Ups!', txt: error.message },
+      data: message,
     });
     return new Promise((resolve) => {
       dialogRef.afterClosed().subscribe((result) => {
