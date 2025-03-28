ee4cf7517fd517f20f28223ef230d41ea15e6e24
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-03-27 19:10:49 -0500

    ok

--- projects/ejflab-front-lib/src/lib/mycommon/pipes/since.pipe.ts
+++ projects/ejflab-front-lib/src/lib/mycommon/pipes/since.pipe.ts
@@ -10,7 +10,8 @@ export class SincePipe implements PipeTransform {
     return new Promise(async (resolve, reject) => {
       if (typeof valueOriginal == 'number') {
         const actual = new Date().getTime();
-        const value = actual - valueOriginal;
+        const signedValue = actual - valueOriginal;
+        const value = Math.abs(signedValue);
         const map: any = {
           'original': valueOriginal,
           'actual': actual,
@@ -19,19 +20,27 @@ export class SincePipe implements PipeTransform {
           'hours': 0,
           'type': 'seconds',
         };
-        map['seconds'] = Math.ceil(value / 1000);
+        map['seconds'] = Math.floor(value / 1000);
         if (map['seconds'] > 60) {
-          map['minutes'] = Math.ceil(map['seconds'] / 60);
+          map['minutes'] = Math.floor(map['seconds'] / 60);
+          map['seconds'] = map['seconds'] % 60;
           map['type'] = 'minutes';
         }
         if (map['minutes'] > 60) {
-          map['hours'] = Math.ceil(map['minutes'] / 60);
+          map['hours'] = Math.floor(map['minutes'] / 60);
+          map['minutes'] = map['minutes'] % 60;
           map['type'] = 'hours';
         }
         let translateFolder: any = 'nogales';
         if (args && args.length > 0) {
           translateFolder = args[0];
         }
+        if (args && args.length > 1) {
+          map['type'] = args[1];
+        }
+        map["hours2"] = (map["hours"]/100).toFixed(2).split(".")[1];
+        map["minutes2"] = (map["minutes"]/100).toFixed(2).split(".")[1];
+        map["seconds2"] = (map["seconds"]/100).toFixed(2).split(".")[1];
         const rendered = await this.translateSrv.translate(`since.${map['type']}`, [translateFolder, map]);
         resolve(rendered);
       } else {
